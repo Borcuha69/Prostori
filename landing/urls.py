49b13198c42094ed936dee70f518bb1cd97ca47f
Borcuha69 <borcuha69@gmail.com>
2020-05-31 20:09:33 +0300

    Оформление с берёзками. Действие второе

--- landing/urls.py
+++ landing/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contacts/$', views.contacts, name='contacts'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def main(requests):
     return render(requests, 'landing/main.html', locals())
+
+def about(requests):
+    return  render(requests, 'landing/about.html', locals())
+
+def contacts(requests):
+    return  render(requests, 'landing/contacts.html', locals())
