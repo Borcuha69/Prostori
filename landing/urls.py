18b9a2ce0c690ce5b9ba965ad8b0e225c3c0372f
Borcuha69 <borcuha69@gmail.com>
2020-06-13 13:36:40 +0300

    add main and modify navbar

--- landing/urls.py
+++ landing/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^about/$', views.about, name='about'),
     url(r'^contacts/$', views.contacts, name='contacts'),
+    url(r'^baths/$', views.baths, name='baths'),
+    url(r'^fermer/$', views.fermer, name='fermer'),
+    url(r'^activity/$', views.activity, name='activity'),
+    url(r'^fishing/$', views.fishing, name='fishing'),
+    url(r'^horsewalking/$', views.horsewalking, name='horsewalking'),
+    url(r'^zoo/$', views.zoo, name='zoo'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -9,3 +9,21 @@ def about(requests):
 
 def contacts(requests):
     return  render(requests, 'landing/contacts.html', locals())
+
+def baths(requests):
+    return  render(requests, 'landing/baths.html', locals())
+
+def fermer(requests):
+    return render(requests, 'landing/fermer.html', locals())
+
+def activity(requests):
+    return  render(requests, 'landing/activity.html', locals())
+
+def fishing(requests):
+    return  render(requests, 'landing/fishing.html', locals())
+
+def horsewalking(requests):
+    return  render(requests, 'landing/horsewalking.html', locals())
+
+def zoo(requests):
+    return  render(requests, 'landing/zoo.html', locals())
