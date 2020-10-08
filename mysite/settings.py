5fd99f24d8e36b6b260c4f92dd065b6ec3ea5981
Borcuha69 <borcuha69@gmail.com>
2020-10-08 14:59:41 +0300

    Prod v1.3

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4)-&(p1g_aer4o$q_xw9%25j2x&+j9aey87kk3p6bkh3ia-q6e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.prostorylife.ru", "webapp-899402.pythonanywhere.com"]
 
 
 # Application definition
